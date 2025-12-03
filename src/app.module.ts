3faa4a5aad8c426270848b9120c321da4ab0fb8b
Wajahat Banday <wajahatbanday@outlook.com>
2025-12-03 12:43:31 +0530

    Added Base Structure For Conventional NestJs Server

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { MercuriusDriver, MercuriusDriverConfig } from '@nestjs/mercurius';
+import { AppResolver } from './app.resolver';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<MercuriusDriverConfig>({
+      driver: MercuriusDriver,
+      autoSchemaFile: true,
+      subscription: true,
+      introspection: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService, AppResolver],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,32 @@
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  try {
+    const app = await NestFactory.create<NestFastifyApplication>(
+      AppModule,
+      new FastifyAdapter(),
+    );
+
+    app.enableCors({
+      origin: '*',
+    });
+
+    const port = 4001;
+    const host = '0.0.0.0';
+
+    await app.listen(port, host);
+
+    console.log('\n🚀 Application is running!');
+    console.log(`📍 Server: http://localhost:${port}`);
+    console.log(`🎯 GraphQL Playground: http://localhost:${port}/graphql`);
+  } catch (error) {
+    console.error('ERROR:', error.message);
+    process.exit(1);
+  }
+}
+bootstrap();
